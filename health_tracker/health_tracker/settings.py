983642003cd0cec8fa92959b675fd213aa0424a5
AQuaintExpression <iliemarian.daniel@gmail.com>
2022-02-07 23:00:44 +0200

    completed basic setup of project app and model

--- health_tracker/health_tracker/settings.py
+++ health_tracker/health_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'weight_tracking.apps.WeightTrackingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- health_tracker/weight_tracking/models.py
+++ health_tracker/weight_tracking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    weight = models.FloatField()
+    entry_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Weight: {self.weight} Date: {self.entry_date}'
